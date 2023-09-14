82b25f85d3de0ca52c39d63deeac10b3f761e185
AlexThukynov1 <tsykunov.oleksandr@gmail.com>
2023-09-14 11:45:38 +0300

    add components,layout ans store

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -78,17 +78,95 @@
         </li>
       </ul>
     </div>
-        <div id="MainHEader" class="flex items-center w-full bg-white">
-            <div class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto">
-                <NuxtLink to="/" class="min-w-[170px]">
-                    <img width="170" src="/Aliexpress-logo.png" alt="">
-                </NuxtLink>
+    <div id="MainHEader" class="flex items-center w-full bg-white">
+      <div
+        class="flex lg:justify-start justify-between gap-10 max-w-[1150px] w-full px-3 py-5 mx-auto"
+      >
+        <NuxtLink to="/" class="min-w-[170px]">
+          <img width="170" src="/Aliexpress-logo.png" alt="" />
+        </NuxtLink>
+        <div class="max-w-[700px] w-full md:block hidden">
+          <div class="relative">
+            <div
+              class="flex items-center border-2 border-[#ff4646] rounded-md w-full"
+            >
+              <input
+                class="w-full placeholder-gray-400 text-sm pl-3 focus:outline-none"
+                type="text"
+                placeholder="kitchen accessories"
+                v-model="searchItem"
+              />
+              <Icon
+                v-if="isSearching"
+                name="eos-icons:loading"
+                size="25"
+                class="mr-2"
+              />
+              <button class="flex items-center h-[100%] p-1.5 bg-[#ff4646]">
+                <Icon name="ph:magnifying-glass" size="20" color="#fff" />
+              </button>
             </div>
+            <div
+              v-if="false"
+              class="absolute bg-white max-w-[700px] h-auto w-full"
+            >
+              <div class="p-1">
+                <NuxtLink
+                  to="`/item/1`"
+                  class="flex items-center justify-between w-full cursor-pointer hover:bg-gray-100"
+                >
+                  <div class="flex items-center">
+                    <img
+                      class="rounded-md"
+                      width="40"
+                      src="https://piscum.photos/id/82/300/320"
+                      alt=""
+                    />
+                    <div class="truncate ml-2">TESTING</div>
+                  </div>
+                  <div class="truncate">$ 98.99</div>
+                </NuxtLink>
+              </div>
+            </div>
+          </div>
         </div>
+        <NuxtLink to="/shoppingcart" class="flex items-center">
+          <button
+            class="relative md:block hidden"
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+          >
+            <span
+              class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#ff4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+            >
+              0
+            </span>
+            <div class="min-w-[40px]">
+              <Icon name="ph:shopping-cart-simple-light" size="33" :color="isCartHover ? '#ff4646' : ''"/>
+            </div>
+          </button>
+        </NuxtLink>
+        <button
+          @click="useStore.isMenuOverlay = true"
+          class="md:hidden block rounded-full p-1.5 mt-[4px] hover:bg-gray-200"
+        >
+          <Icon name="radix-icons:hamburger-menu" size="33" />
+        </button>
+      </div>
+    </div>
   </div>
+  <Loading v-if="useStore.isLoading"/>
+  <div class="lg:pt-[150px] md:pt-[130px] pt-[80px]"></div>
   <slot />
+  <Footer v-if="!useStore.isLoading"/>
 </template>
 
 <script setup>
+import {useUserStore} from '~/stores/user'
+const useStore = useUserStore()
+
 let isAccountMenu = ref(false);
+let searchItem = ref("");
+let isSearching = ref(false);
+let isCartHover = ref(false);
 </script>
